0046f843c8ea0df617546432c53536257d0f7110
ahmed3moustafa <47308345+ahmed3moustafa@users.noreply.github.com>
2020-10-14 19:37:12 +0200

    Update fuff_automation.py

--- fuff_automation.py
+++ fuff_automation.py
@@ -35,6 +35,7 @@ def ffuf(urls, wordlist, no):
 
 
 def first_urls(all_urls, start_line, end_line):
+    first_lines = []
     with open(all_urls, "r") as urls:
         first_lines = list(islice(urls, start_line, end_line))
     return first_lines
